4e74449179abeca23b8d50062078a30736e2eae0
CharanBlaze <charan@blazeautomation.com>
2026-05-21 23:44:32 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,40 @@
 // swift-tools-version:5.7
+
 import PackageDescription
 
 let package = Package(
     name: "DoorbellSDK",
-    platforms: [.iOS(.v14)],
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "DoorbellSDK",
-            targets: ["DoorbellSDK"]
+            targets: ["DoorbellSDKWrapper"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/stasel/WebRTC.git",
+            from: "147.0.0"
         )
     ],
     targets: [
+
+        // Binary XCFramework
         .binaryTarget(
             name: "DoorbellSDK",
             url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.0/DoorbellSDK.zip",
             checksum: "f8ca0d3c8971bbfaf6a927713677d68f2f1d4db7102b3f1af1f5dcba0235ed2d"
+        ),
+
+        // Wrapper target to expose dependencies
+        .target(
+            name: "DoorbellSDKWrapper",
+            dependencies: [
+                "DoorbellSDK",
+                .product(name: "WebRTC", package: "WebRTC")
+            ]
         )
     ]
 )
