45d48ca470f78a9c0fbc93a3ce88f5fb60625b8b
CharanBlaze <charan@blazeautomation.com>
2026-05-25 12:31:12 +0530

    checksum Package.swift

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         // Binary XCFramework
         .binaryTarget(
             name: "DoorbellSDK",
-            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.1/DoorbellSDK-1.0.1.zip",
-            checksum: "52b51faa2833fe523e9219bdb2f7d5428f2f81f28a4957398a6fa6d8006dd978"
+            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.2/DoorbellSDK_1_0_2.zip",
+            checksum: "af0960e5a41f777a47822d8eb5ff9561733b686869642926b104fd450acb417b"
         ),
 
         // Wrapper target to expose dependencies
