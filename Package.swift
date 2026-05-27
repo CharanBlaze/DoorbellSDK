51035b06b42db6f553fa85d3aecc3fb58108d403
CharanBlaze <charan@blazeautomation.com>
2026-05-27 14:41:31 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         // This name must exactly match the name of the .xcframework folder inside your zip.
         .binaryTarget(
             name: "DoorbellSDK",
-            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.6/DoorbellSDK_1_0_6.zip",
-            checksum: "18dbe32cd00b4fad8c7c5c5276b38a241ccbb5c5fe8f79158c32dc5a1328a312"
+            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.8/DoorbellSDK_1_0_8.zip",
+            checksum: "cd55a7bbb8f71f576e1fbb8c568bdc687ae4e3692e073144e7a88de2f6d00f0c"
         ),
         // 2. The wrapper target which glues your binary and WebRTC together.
         .target(
