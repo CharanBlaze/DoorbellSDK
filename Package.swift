8e3674dad8b6c1cf9ceae863a783b63eb5d3d6bb
CharanBlaze <charan@blazeautomation.com>
2026-05-29 12:53:50 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "DoorbellSDK",
-            targets: ["DoorbellSDKWrapper"] // Clients will link to this wrapper
+            targets: ["DoorbellSDKWrapper"]
         )
     ],
     dependencies: [
@@ -19,21 +19,18 @@ let package = Package(
         )
     ],
     targets: [
-        // 1. The precompiled binary target.
-        // This name must exactly match the name of the .xcframework folder inside your zip.
         .binaryTarget(
             name: "DoorbellSDK",
-            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.8/DoorbellSDK_1_0_8.zip",
-            checksum: "cd55a7bbb8f71f576e1fbb8c568bdc687ae4e3692e073144e7a88de2f6d00f0c"
+            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.10/DoorbellSDK_1_0_10.zip",
+            checksum: "59b193b0168c4b15babefa5375c9d270bed83b6443278d3f71110074476401d2"
         ),
-        // 2. The wrapper target which glues your binary and WebRTC together.
         .target(
             name: "DoorbellSDKWrapper",
             dependencies: [
-                "DoorbellSDK", // References the binary target above
-                .product(name: "WebRTC", package: "WebRTC") // References the WebRTC dependency
+                "DoorbellSDK", 
+                .product(name: "WebRTC", package: "WebRTC")
             ],
-            path: "Sources/DoorbellSDKWrapper" // Explicit path for the wrapper
+            path: "Sources/DoorbellSDKWrapper"
         )
     ]
 )
