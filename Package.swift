89765b9036b032a6f62306613b11533e528ba9f1
CharanBlaze <charan@blazeautomation.com>
2026-07-14 18:24:53 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DoorbellSDK",
-            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.26/DoorbellSDK_1_0_26.zip",
-            checksum: "1bb1bafa6a77286978518804373c03a619cd55437e968124596cd875a5de2e3e"
+            url: "https://github.com/CharanBlaze/DoorbellSDK/releases/download/1.0.28/DoorbellSDK_1_0_28.zip",
+            checksum: "5f1c6bbcdad0e527f37387e30a9b5a27021ba7f4a7e377edf2266595e240882f"
         ),
         .target(
             name: "DoorbellSDKWrapper",
